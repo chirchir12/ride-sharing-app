f656d2fb81472e4f8a26a9285bee621cbe4f5577
chirchir <chirchir7370@gmail.com>
2023-08-27 19:42:56 +0300

    additional auth errors

--- src/auth/errors.ts
+++ src/auth/errors.ts
@@ -2,4 +2,5 @@ export const authErrors = {
   userExist: 'User with phone/email exist',
   unAuthorized: 'unAuthorized',
   invalidUsernamePassword: 'Invalid Username or Password',
+  driverExist: 'Driver is already registered',
 };
