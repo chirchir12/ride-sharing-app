3b141d86b28bd626f14608e8518ddd9e445d4958
chirchir <chirchir7370@gmail.com>
2023-08-27 20:02:03 +0300

    import driver entity into typeorm config

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DriverService } from './drivers.service';
 import { DriverRepository } from './drivers.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DriverEntity } from './drivers.entity';
+import { DriverController } from './drivers.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DriverEntity])],
   providers: [DriverRepository, DriverService],
   exports: [DriverService],
+  controllers: [DriverController],
 })
 export class DriversModule {}
